e5f668ab5a132a80ab1f77990b18870d4a4524e1
jypark <rjseka1540@gmail.com>
2022-09-01 22:22:13 +0900

    초기 커밋.

---
+++ TcpIpTestProgram/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcpIpTestProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+
+            if (bt.Text == "button1")
+            {
+                MessageBox.Show("Button 1");
+            }
+            else
+            {
+                MessageBox.Show("Button 2");
+            }
+
+        }
+    }
+}
